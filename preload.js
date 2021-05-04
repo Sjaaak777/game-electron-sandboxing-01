489b6891fae8b75d0774b73285014a24ff693542
unknown <sjaaak777@gmail.com>
2021-05-04 19:53:46 +0200

    refactor code

--- preload.js
+++ preload.js
@@ -1,5 +1,4 @@
 const THREE = require('three')
-
 const OrbitControls = require('three-orbit-controls')(THREE)
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -12,6 +11,20 @@ window.addEventListener('DOMContentLoaded', () => {
     1000
   )
 
+  // RENDERER
+  const renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  document.body.appendChild(renderer.domElement)
+
+  // ORBITCONTROLS
+  controls = new OrbitControls(camera)
+  controls.enableDamping = true
+  controls.dampingFactor = 0.1
+
+  // AXESHELPER
+  const axesHelper = new THREE.AxesHelper(3)
+  scene.add(axesHelper)
+
   // LIGHT
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
   scene.add(ambientLight)
@@ -20,40 +33,31 @@ window.addEventListener('DOMContentLoaded', () => {
   pointLight.position.x = 2
   pointLight.position.y = 3
   pointLight.position.z = 4
-
   scene.add(pointLight)
-
-  const renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
-
-  controls = new OrbitControls(camera)
-  controls.enableDamping = true
-  controls.dampingFactor = 0.1
 
   // FLOOR
   const floorGeometry = new THREE.BoxGeometry(20, 0.05, 20)
   const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x74e6e1 })
   const floor = new THREE.Mesh(floorGeometry, floorMaterial)
-  floor.position.y = -0.5
+  floor.position.y = -0.05
   scene.add(floor)
 
   // BOX
-  const geometry = new THREE.BoxGeometry()
+  const geometry = new THREE.BoxGeometry(1, 1, 1)
   const boxMaterial = new THREE.MeshStandardMaterial({ color: 0xe67479 })
   const cube = new THREE.Mesh(geometry, boxMaterial)
   scene.add(cube)
 
-  const axesHelper = new THREE.AxesHelper(3)
-  scene.add(axesHelper)
-
+  cube.position.x = 1
   cube.position.y = 1
+  cube.position.z = 1
 
   // CAMERA
   camera.position.z = 5
   camera.position.x = 2
   camera.position.y = 1
 
+  // ANIMATE
   const animate = function () {
     requestAnimationFrame(animate)
 
